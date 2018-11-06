3d7a0b6b0308036d1ab206041c4abd8a97de11a1
RhondaStark92 <rhondastark@comcast.net>
2018-11-05 18:29:56 -0600

    heroku changes for react

---
+++ build/precache-manifest.21e5714e26f85643a8b0eee57a6875e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e7cbd0e58763c6697067",
+    "url": "/static/js/main.e7cbd0e5.chunk.js"
+  },
+  {
+    "revision": "1b62a4b37caa815f7ca9",
+    "url": "/static/js/1.1b62a4b3.chunk.js"
+  },
+  {
+    "revision": "e7cbd0e58763c6697067",
+    "url": "/static/css/main.0a5ca56b.chunk.css"
+  },
+  {
+    "revision": "e2ca22c919791ed47da74126fdf4eb94",
+    "url": "/index.html"
+  }
+];
